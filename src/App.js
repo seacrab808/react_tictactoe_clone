5e08689a957053fc1ff1513e5dd074c9b7c60ce3
seacrab808 <seacrab808@naver.com>
2023-09-29 02:27:57 +0900

    다 만듬

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import './App.css';
-import Board from './components/Board';
+import { useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
-
-  const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
   const calculateWinner = (squares) => {
@@ -16,46 +16,69 @@ function App() {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
-    ]
+      [2, 4, 6],
+    ];
     for (let index = 0; index < lines.length; index++) {
       const [a, b, c] = lines[index];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
         return squares[a];
       }
     }
     return null;
-  }  
+  };
 
-  const current = history[history.length - 1];
+  const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
 
-
   let status;
-  if(winner) {
-    status = 'Winner: ' + winner;
+  if (winner) {
+    status = "Winner: " + winner;
   } else {
-    status = `Next Player: ${xIsNext? 'X' : 'O'}`;
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
-    if(calculateWinner(newSquares) || newSquares[i]) {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
       return;
     }
-    newSquares[i] = xIsNext ? 'X' : 'O';
-    setHistory([...history, {squares: newSquares}]);
-    setXIsNext(current => !current);
-  }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...newHistory, { squares: newSquares }]);
+    setXIsNext((current) => !current);
 
+    setStepNumber(newHistory.length);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move#" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} onClick={(i) => handleClick(i)}/>
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div className='status'>{status}</div>
+        <div className="status">{status}</div>
+        <ol style={{ listStyle: "none" }}>{moves}</ol>
       </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,38 +1,31 @@
-
-
-import React, { useState } from 'react';
-import Square from './Square';
+import React from "react";
+import Square from "./Square";
 import "./Board.css";
 
-const Board = ({squares, onClick}) => {
-  
+const Board = ({ squares, onClick }) => {
   const renderSquare = (i) => {
-    return <Square
-              value={squares[i]} 
-              onClick={() => onClick(i)} 
-              />
-  }
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  };
 
-    return (
-      <div>
-        <div className='board-row'>
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className='board-row'>
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className='board-row'>
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
+  return (
+    <div className="board-wrapper">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    )
-  }
-  
-  export default Board
-  
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
